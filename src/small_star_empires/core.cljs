f563510385bf0656ffbcc1074d7226963b6cff99
Arnauld <arnauld.loyer@gmail.com>
2016-04-01 20:10:41 +0200

    add svg rendering

--- src/small_star_empires/core.cljs
+++ src/small_star_empires/core.cljs
@@ -1,5 +1,8 @@
 (ns small_star_empires.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require
+    [reagent.core :as reagent :refer [atom]]
+    [clojure.string :as string]))
+
 
 (enable-console-print!)
 
@@ -9,8 +12,31 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn rgb [color]
+  "#181818")
+
+(defn block [x y color]
+  [:rect {:x            x
+          :y            y
+          :width        1
+          :height       1
+          :stroke       "black"
+          :stroke-width 0.01
+          :rx           0.1
+          :fill         (rgb color)}])
+
+(defn board-view []
+  (let [x 1]
+    [:svg {:style {:border "1px solid black"
+                   :width 400
+                   :height 300}
+           :view-box (string/join " " [0 0 10 20])}
+     ]))
+
 (defn hello-world []
-  [:h1 (:text @app-state)])
+  [:h1 (:text @app-state)
+   [:br]
+   [board-view]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
@@ -19,5 +45,5 @@
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (swap! app-state update-in [:__figwheel_counter] inc)
+  )
