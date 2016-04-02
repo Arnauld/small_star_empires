5c9b9d39627e245f77bf945d4f515c347a772c33
Arnauld <arnauld.loyer@gmail.com>
2016-04-02 17:54:47 +0200

    draw some hexes

--- src/small_star_empires/core.cljs
+++ src/small_star_empires/core.cljs
@@ -13,7 +13,12 @@
 (defonce app-state (atom {:text "Hello world!"}))
 
 (defn rgb [color]
-  "#181818")
+  (case color
+    :pink "#ff69b4"
+    :orange "#ffa500"
+    :darkblue "#000080"
+    :darkred "#8b0000"
+    "#181818"))
 
 (defn block [x y color]
   [:rect {:x            x
@@ -25,12 +30,48 @@
           :rx           0.1
           :fill         (rgb color)}])
 
+(def PI_D3 (/ Math/PI 3))
+(def HEX (map (fn [i] [(Math/cos (* i PI_D3))
+                       (Math/sin (* i PI_D3))])
+              (range 0 6)))
+
+(defn hex-points [x y radius]
+  (map (fn [[hx hy]] [(+ x (* radius hx))
+                      (+ y (* radius hy))])
+       HEX))
+
+(defn format-points [points]
+  (reduce (fn [acc [x y]] (str acc " " (int x) "," (int y))) "" points))
+
+(defn draw-hex [x y radius color]
+  [:polygon {:style  {:fill            (rgb color)
+                      :stroke          "black"
+                      :stroke-width    "4px"
+                      :stroke-linejoin "round"}
+             :points (format-points (hex-points x y radius))}])
+
 (defn board-view []
-  (let [x 1]
-    [:svg {:style {:border "1px solid black"
-                   :width 400
-                   :height 300}
-           :view-box (string/join " " [0 0 10 20])}
+  (let [radius 40
+        width (* 2 radius)
+        height (* width (/ (Math/sqrt 3) 2))
+        deltaX (* width (/ 3 4))
+        deltaX2 (* 2 deltaX)
+        deltaY (/ height 2)
+        deltaY2 (* 2 deltaY)
+        x 40
+        y 40]
+    [:svg {:style    {:border "1px solid black"
+                      :width  400
+                      :height 300}
+           :view-box (string/join " " [0 0 400 400])}
+     (draw-hex x y radius :pink)
+     (draw-hex x (+ y deltaY2) radius :darkred)
+     (draw-hex (+ x deltaX) (+ y deltaY) radius :orange)
+     (draw-hex (+ x deltaX2) y radius :darkblue)
+     (draw-hex (+ x deltaX2) (+ y deltaY2) radius :darkred)
+     (draw-hex (+ x deltaX2) (+ y deltaY2 deltaY2) radius :darkblue)
+     (draw-hex (+ x deltaX2 deltaX2) y radius :pink)
+     (draw-hex (+ x deltaX2 deltaX2 deltaX2) y radius :pink)
      ]))
 
 (defn hello-world []
