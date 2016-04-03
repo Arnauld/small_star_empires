82d87aff07ff39bb32308e886ec69cc6d408ace1
Arnauld <arnauld.loyer@gmail.com>
2016-04-03 23:01:01 +0200

    last react version to fix svg attributes missing on feGaussianBlur + filter on element

--- project.clj
+++ project.clj
@@ -2,16 +2,19 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.6.1"
-  
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.async "0.2.374"
                   :exclusions [org.clojure/tools.reader]]
-                 [reagent "0.5.1"]]
-  
+                 ; last reagent + last react...
+                 [reagent "0.6.0-alpha" :exclusions [cljsjs/react-dom]]
+                 [cljsjs/react-dom "15.0.0-rc.2-0"]
+                 [cljsjs/react-dom-server "15.0.0-rc.2-0"]]
+
   :plugins [[lein-figwheel "0.5.2"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
@@ -20,32 +23,32 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :cljsbuild {:builds
-              [{:id "dev"
+              [{:id           "dev"
                 :source-paths ["src"]
 
                 ;; If no code is to be run, set :figwheel true for continued automagical reloading
-                :figwheel {:on-jsload "small_star_empires.core/on-js-reload"}
+                :figwheel     {:on-jsload "small_star_empires.core/on-js-reload"}
 
-                :compiler {:main small_star_empires.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/small_star_empires.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true}}
+                :compiler     {:main                 small_star_empires.core
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/small_star_empires.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true}}
                ;; This next build is an compressed minified build for
                ;; production. You can build this with:
                ;; lein cljsbuild once min
-               {:id "min"
+               {:id           "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/small_star_empires.js"
-                           :main small_star_empires.core
-                           :optimizations :advanced
-                           :pretty-print false}}]}
+                :compiler     {:output-to     "resources/public/js/compiled/small_star_empires.js"
+                               :main          small_star_empires.core
+                               :optimizations :advanced
+                               :pretty-print  false}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs ["resources/public/css"]             ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
