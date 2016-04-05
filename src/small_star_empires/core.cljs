3f04d612aff9cd78a011f929053b6ae7bf431aab
Arnauld <arnauld.loyer@gmail.com>
2016-04-06 00:30:09 +0200

    attempt to group svg elements into g element + simplify all draw variation with styles management

--- src/small_star_empires/core.cljs
+++ src/small_star_empires/core.cljs
@@ -3,7 +3,6 @@
     [reagent.core :as reagent :refer [atom]]
     [clojure.string :as string]))
 
-
 (enable-console-print!)
 
 (println "Edits to this text should show up in your developer console.")
@@ -51,111 +50,74 @@
 (defn format-points [points]
   (reduce (fn [acc [x y]] (str acc " " x "," y)) "" points))
 
-(defn fill-hex
+(defn hex
   ([x y radius color]
-   (fill-hex x y radius color 0))
-  ([x y radius color rounded-radius]
-   (let [radiusToUse (- radius (* 0.75 rounded-radius))]
-     [:polygon {:style  {:fill            (rgb color)
+   (hex x y radius color {}))
+  ([x y radius color styles]
+   (let [rounded-radius (get styles :rounded-radius 0)
+         radiusToUse (- radius (* 0.75 rounded-radius))
+         default-styles {:fill            (rgb color)
                          :stroke          (rgb color)
                          :stroke-width    (str rounded-radius "px")
-                         :stroke-linejoin "round"}
+                         :stroke-linejoin "round"}]
+
+     [:polygon {:style  (merge default-styles styles)
                 :points (format-points (hex-points x y radiusToUse))}])))
 
-(defn draw-hex
+
+(defn circle
   ([x y radius color]
-   (draw-hex x y radius color 2))
-  ([x y radius color rounded-radius]
-   (let [radiusToUse (- radius (* 0.75 rounded-radius))]
-     [:polygon {:style  {:fill            "none"
-                         :stroke          (rgb color)
-                         :stroke-width    (str rounded-radius "px")
-                         :stroke-linejoin "round"}
-                :points (format-points (hex-points x y radiusToUse))}])))
-
-(defn draw-hex-dashed
-  ([x y radius color]
-   (draw-hex-dashed x y radius color 2))
-  ([x y radius color rounded-radius]
-   (let [radiusToUse (- radius (* 0.75 rounded-radius))]
-     [:polygon {:style  {:fill             "none"
-                         :stroke           (rgb color)
-                         :stroke-width     (str rounded-radius "px")
-                         :stroke-linejoin  "round"
-                         :stroke-dasharray (string/join "," [(/ radius 5) (/ radius 5)])}
-                :points (format-points (hex-points x y radiusToUse))}])))
-
-(defn fill-circle
-  ([x y radius color]
-   (fill-circle x y radius color 2))
-  ([x y radius color rounded-radius]
-   [:circle {:style {:fill            (rgb color)
-                     :stroke          (rgb color)
-                     :stroke-width    (str rounded-radius "px")
-                     :stroke-linejoin "round"}
-             :cx    x
-             :cy    y
-             :r     radius}]))
-
-(defn blurred-circle
-  ([x y radius color mode]
-   (blurred-circle x y radius color mode 2))
-  ([x y radius color mode rounded-radius]
-   [:circle {:style {:fill         (rgb color)
-                     :stroke       (rgb color)
-                     :stroke-width (str rounded-radius "px")
-                     :filter       (str "url(#" mode ")")}
-             :cx    x
-             :cy    y
-             :r     radius}]))
-
-(defn draw-circle
-  ([x y radius color]
-   (draw-circle x y radius color 2))
-  ([x y radius color rounded-radius]
-   [:circle {:style {:fill            "none"
-                     :stroke          (rgb color)
-                     :stroke-width    (str rounded-radius "px")
-                     :stroke-linejoin "round"}
-             :cx    x
-             :cy    y
-             :r     radius}]))
-
+   (circle x y radius color {}))
+  ([x y radius color styles]
+   (let [default-styles {:fill         (rgb color)
+                         :stroke       (rgb color)
+                         :stroke-width 2}]
+     [:circle {:style (merge default-styles styles)
+               :cx    x
+               :cy    y
+               :r     radius}])))
 
 (defn draw-planet-orbit
   ([x y orbit-radius orbit-angle]
-    (draw-planet-orbit x y orbit-radius orbit-angle :grey :white))
+   (draw-planet-orbit x y orbit-radius orbit-angle :grey :white))
   ([x y orbit-radius orbit-angle orbit-color planet-color]
-   [(draw-circle x y orbit-radius orbit-color 1)
-    (fill-circle (+ x (* orbit-radius (Math/cos orbit-angle)))
-                 (+ y (* orbit-radius (Math/sin orbit-angle))) 4 :black)
-    (fill-circle (+ x (* orbit-radius (Math/cos orbit-angle)))
-                 (+ y (* orbit-radius (Math/sin orbit-angle))) 2 planet-color)]))
+   [(circle x y orbit-radius orbit-color {:fill         "none"
+                                          :stroke-width 1})
+    (circle (+ x (* orbit-radius (Math/cos orbit-angle)))
+            (+ y (* orbit-radius (Math/sin orbit-angle))) 4 :black)
+    (circle (+ x (* orbit-radius (Math/cos orbit-angle)))
+            (+ y (* orbit-radius (Math/sin orbit-angle))) 2 planet-color)]))
 
 (defn draw-homeworld [x y]
   (let [homeworld-color :homeworld-green
         ANG1 (* TWO_PI 0.66)
         ANG2 (* TWO_PI 0.12)]
-    (-> [(fill-hex x y 40 :black)
-         (draw-hex-dashed x y 35 homeworld-color)
-         (draw-circle x y 15 :grey 1)
-         (fill-circle x y 1 homeworld-color)]
+    (-> [(hex x y 40 :black)
+         (hex x y 35 homeworld-color {:fill             "none"
+                                      :rounded-radius   2
+                                      :stroke-dasharray "7,7"})
+         (circle x y 15 :grey {:fill         "none"
+                               :stroke-width 1})
+         (circle x y 1 homeworld-color)]
         (into (draw-planet-orbit x y 15 ANG1 homeworld-color homeworld-color))
         (into (draw-planet-orbit x y 22 ANG2 homeworld-color homeworld-color)))))
 
 (defn draw-planet [x y]
-  (-> [(fill-hex x y 40 :black)
-       (draw-hex x y 35 :grey)
-       (draw-circle x y 15 :grey 1)
-       (fill-circle x y 1 :white)]
+  (-> [(hex x y 40 :black)
+       (hex x y 35 :grey {:fill           "none"
+                          :rounded-radius 2})
+       (circle x y 15 :grey {:fill         "none"
+                             :stroke-width 1})
+       (circle x y 1 :white)]
       (into (draw-planet-orbit x y 15 PI_D3))))
 
 (defn draw-planet-2 [x y]
   (let [ANG2 (/ TWO_PI 2)
         ANG3 (* TWO_PI 0.8)]
-    (-> [(fill-hex x y 40 :black)
-         (draw-hex x y 35 :grey)
-         (fill-circle x y 1 :white)]
+    (-> [(hex x y 40 :black)
+         (hex x y 35 :grey {:fill           "none"
+                            :rounded-radius 2})
+         (circle x y 1 :white)]
         (into (draw-planet-orbit x y 15 ANG2))
         (into (draw-planet-orbit x y 22 ANG3)))))
 
@@ -163,27 +125,29 @@
   (let [ANG1 (/ TWO_PI 7)
         ANG2 (/ TWO_PI 2)
         ANG3 (* TWO_PI 0.8)]
-    (-> [(fill-hex x y 40 :black)
-         (draw-hex x y 35 :grey)
-         (fill-circle x y 1 :white)]
+    (-> [(hex x y 40 :black)
+         (hex x y 35 :grey {:fill           "none"
+                            :rounded-radius 2})
+         (circle x y 1 :white)]
         (into (draw-planet-orbit x y 10 ANG2))
         (into (draw-planet-orbit x y 17 ANG3))
         (into (draw-planet-orbit x y 24 ANG1)))))
 
 (defn draw-nova [x y nova-color]
-  (-> [(fill-hex x y 40 :black)
-       (draw-hex x y 35 :grey)]
-      (into [(blurred-circle x y 10 nova-color "blur1")
-             (blurred-circle x y 5 :white "blur4")
-             (blurred-circle x (- y 13) 10 nova-color "blur1")
-             (blurred-circle x (- y 13) 5 :white "blur4")
-             (blurred-circle (+ x 15) (- y 5) 8 nova-color "blur2")
-             (blurred-circle (- x 15) (- y 12) 8 nova-color "blur2")
-             (blurred-circle (- x 15) (- y 12) 4 :white "blur4")
-             (blurred-circle (- x 10) (- y 7) 4 :white "blur4")
-             (blurred-circle (- x 2) (+ y 15) 10 nova-color "blur3")
-             (blurred-circle (- x 2) (+ y 15) 4 :white "blur4")
-             ])))
+  [:g {:class "nova"}
+   (hex x y 40 :black)
+   (hex x y 35 :grey {:fill           "none"
+                      :rounded-radius 2})
+   (circle x y 10 nova-color {:filter "url(#blur1)"})
+   (circle x y 5 :white {:filter "url(#blur4"})
+   (circle x (- y 13) 10 nova-color {:filter "url(#blur1"})
+   (circle x (- y 13) 5 :white {:filter "url(#blur4"})
+   (circle (+ x 15) (- y 5) 8 nova-color {:filter "url(#blur2"})
+   (circle (- x 15) (- y 12) 8 nova-color {:filter "url(#blur2"})
+   (circle (- x 15) (- y 12) 4 :white {:filter "url(#blur4"})
+   (circle (- x 10) (- y 7) 4 :white {:filter "url(#blur4"})
+   (circle (- x 2) (+ y 15) 10 nova-color {:filter "url(#blur3"})
+   (circle (- x 2) (+ y 15) 4 :white {:filter "url(#blur4"})])
 
 (defn board-view []
   (let [radius 40
@@ -229,14 +193,14 @@
                       ["feGaussianBlur" {:in           "SourceGraphic"
                                          :stdDeviation "3"}]]])
         (into
-          [(fill-hex x y radius :pink)
-           (fill-hex x (+ y deltaY2) radius :darkred)
-           (fill-hex (+ x deltaX) (+ y deltaY) radius :orange)
-           (fill-hex (+ x deltaX2) y radius :darkblue)
-           (fill-hex (+ x deltaX2) (+ y deltaY2) radius :darkred)
-           (fill-hex (+ x deltaX2) (+ y deltaY2 deltaY2) radius :darkblue)
-           (fill-hex (+ x deltaX2 deltaX2) y radius :pink)
-           (fill-hex (+ x deltaX2 deltaX2 deltaX2) y radius :pink)])
+          [(hex x y radius :pink)
+           (hex x (+ y deltaY2) radius :darkred)
+           (hex (+ x deltaX) (+ y deltaY) radius :orange)
+           (hex (+ x deltaX2) y radius :darkblue)
+           (hex (+ x deltaX2) (+ y deltaY2) radius :darkred)
+           (hex (+ x deltaX2) (+ y deltaY2 deltaY2) radius :darkblue)
+           (hex (+ x deltaX2 deltaX2) y radius :pink)
+           (hex (+ x deltaX2 deltaX2 deltaX2) y radius :pink)])
         (into
           (draw-planet
             (+ x deltaX2 deltaX2) (+ y deltaY2)))
@@ -249,13 +213,13 @@
         (into
           (draw-planet-3
             (+ x deltaX2 deltaX2 deltaX) (+ y deltaY2 deltaY)))
-        (into
+        (conj
           (draw-nova
             (+ x deltaX2 deltaX2) (+ y deltaY2 deltaY2 deltaY2) :nova-red))
-        (into
+        (conj
           (draw-nova
             (+ x deltaX2 deltaX2 deltaX) (+ y deltaY2 deltaY2 deltaY2 deltaY) :nova-blue))
-        (into
+        (conj
           (draw-nova
             (+ x deltaX2 deltaX2 deltaX) (+ y deltaY2 deltaY2 deltaY) :nova-green))
         )))
